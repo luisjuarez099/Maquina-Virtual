4b2a628ec94bc353a74fcf6572f576eb7f754c1d
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-05-01 22:04:32 -0500

    Numeros omitidos divi. entre 3

--- list_comprehension.py
+++ list_comprehension.py
@@ -2,10 +2,13 @@ resl=0
 def run():
     # nums=[1,2,3,4,5,6,7,8,9,10]
     nums_exp=[]
-    for i in range(0,100+1):
+    for i in range(0,21):
         resl=i**2
+        if resl%3 ==0:
+            continue
         nums_exp.append(resl)
     print(nums_exp)
+    
 
 
 if __name__=='__main__': #iniciar funcion alejectar archivo de python.
