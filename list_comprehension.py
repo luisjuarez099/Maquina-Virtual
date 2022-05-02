381be43b3ca2d03f401fd0ad0593ef7e864d9b10
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-05-01 23:19:39 -0500

    Quitamos comentarios

--- list_comprehension.py
+++ list_comprehension.py
@@ -7,7 +7,7 @@ def run():
     #         continue
     #     nums_exp.append(resl)
     # nums_exp=[pow(i,2) for i in range(1,21) if i%3 !=0]
-    nums_exp=[i for i in range(1,601) if i%4==0 and i%6==0 and i%9==0]
+    nums_exp=[i for i in range(1,301) if i%4==0 and i%6==0 and i%9==0]
     print(nums_exp)
 
 if __name__=='__main__': 
