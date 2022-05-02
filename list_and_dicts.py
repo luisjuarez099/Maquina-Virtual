2d3646d8f7833cf5bc23dbf31f0ff2efb1395878
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-05-01 19:25:06 -0500

    python super list and dicts

---
+++ list_and_dicts.py
@@ -0,0 +1,22 @@
+def run():
+    my_list=[1,"Juarez",True, 21.12]
+    my_dict={"Name":"Luis",
+            "Age" : 23}
+
+    super_list=[
+            {"Name":"Luis",
+            "Age" : 23,
+            "Name":"Lalo",
+            "Age" : 24,
+            "Name":"Axiel",
+            "Age" : 33,
+            "Name":"Valeria",
+            "Age" : 22}]
+    super_dict={
+        "Float nums": [1.2,2.3,3.4,4.5,5.6],
+        "Integer nums": [1,2,3,4,5,6],
+        "Par nums":[2,4,6,8]
+    }
+
+if __name__=='__main__': #iniciar funcion alejectar archivo de python.
+    run()
