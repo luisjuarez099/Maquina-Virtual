5942b1b9ed1bbcf6cd4cdeecec2437bf81fc07b9
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-05-01 21:44:12 -0500

    list comprehension

---
+++ list_comprehension.py
@@ -0,0 +1,10 @@
+resl=0
+def run():
+    nums=[1,2,3,4,5,6,7,8,9,10]
+    for i in nums:
+        resl=i**2
+        print(resl)
+
+
+if __name__=='__main__': #iniciar funcion alejectar archivo de python.
+    run()
