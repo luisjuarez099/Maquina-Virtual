ecebc669cd781440ac5d64810483f39828be23da
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-05-01 21:49:13 -0500

    Cremaos nueva lista con respuestas de exponentes usando for

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,9 +1,11 @@
 resl=0
 def run():
     nums=[1,2,3,4,5,6,7,8,9,10]
+    nums_exp=[]
     for i in nums:
         resl=i**2
-        print(resl)
+        nums_exp.append(resl)
+    print(nums_exp)
 
 
 if __name__=='__main__': #iniciar funcion alejectar archivo de python.
