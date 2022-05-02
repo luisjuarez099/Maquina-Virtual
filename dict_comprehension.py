6497b691075b582af5cf696ed74f1e636c39e055
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-05-02 18:19:39 -0500

    Dict comprehension omitir divisibles entre 3

--- dict_comprehension.py
+++ dict_comprehension.py
@@ -1,9 +1,6 @@
 def run():
-    dict={}
-    for i in range(1,11):
-        if i%3==0:
-            continue
-        dict[i]=pow(i,3)
-    print(dict)
+    nums_exp={i: pow(i,3) for i in range(1,20)  if i%3 != 0 }
+    print(nums_exp)
+
 if __name__=='__main__':
     run()
