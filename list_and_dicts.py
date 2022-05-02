77de7adf1de6e7bccecd62cdfb18f01c13f4def5
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-05-01 21:08:36 -0500

    creamos for para super listas y diccionarios

--- list_and_dicts.py
+++ list_and_dicts.py
@@ -1,22 +1,21 @@
 def run():
     my_list=[1,"Juarez",True, 21.12]
-    my_dict={"Name":"Luis",
-            "Age" : 23}
+    my_dict={"Name":"Luis", "Lastname" : "juarez"}
 
-    super_list=[
-            {"Name":"Luis",
-            "Age" : 23,
-            "Name":"Lalo",
-            "Age" : 24,
-            "Name":"Axiel",
-            "Age" : 33,
-            "Name":"Valeria",
-            "Age" : 22}]
+    super_list=[{"Name":"Luis", "Age": "25", "Name":"Edgar", "Age": "22"}]
+
     super_dict={
         "Float nums": [1.2,2.3,3.4,4.5,5.6],
         "Integer nums": [1,2,3,4,5,6],
         "Par nums":[2,4,6,8]
     }
+    #super diccionario
+    for keys, value in super_dict.items():
+        print(keys, " : ", value)
+    #super lista
+    for items in super_list:
+        for k,v in items.items():
+	        print(''.join(f"{k} {v}"))
 
 if __name__=='__main__': #iniciar funcion alejectar archivo de python.
     run()
