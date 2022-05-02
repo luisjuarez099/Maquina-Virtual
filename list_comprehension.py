46533701af98d4dc0662b88a084e23938a096955
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-05-02 17:44:24 -0500

    comentario del LstCmpre.

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,5 +1,9 @@
 resl=0
 def run():
+    '''
+    Usamos una list comprehension para conocer los numeros
+    divisibles entre 4 , 6 y 9 hasta llegar al mil.
+    '''
     nums_exp=[i for i in range(1,601) if i%4==0 and i%6==0 and i%9==0]
     print(nums_exp)
 
