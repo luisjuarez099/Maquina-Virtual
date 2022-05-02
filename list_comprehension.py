6ffafb8d76b36764563b4a7732d6ad09e1952c0f
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-05-01 22:44:00 -0500

    List Cmpre. nmeros divi entre 4,6 y 9

--- list_comprehension.py
+++ list_comprehension.py
@@ -6,10 +6,9 @@ def run():
     #     if resl%3 ==0:
     #         continue
     #     nums_exp.append(resl)
-    nums_exp=[pow(i,2) for i in range(1,21) if i%3 !=0]
+    # nums_exp=[pow(i,2) for i in range(1,21) if i%3 !=0]
+    nums_exp=[i for i in range(1,601) if i%4==0 and i%6==0 and i%9==0]
     print(nums_exp)
-    
 
-
-if __name__=='__main__': #iniciar funcion alejectar archivo de python.
+if __name__=='__main__': 
     run()
