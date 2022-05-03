62ba2dcf4e8281b0dba8cb34146c3c5785fbfd7d
LuisJuarez@gmail.com <21al7207485@unedl.mx>
2022-05-03 10:27:46 -0500

    Cramos una lits compreh. para numeros al cuadrado

--- dict_comprehension.py
+++ dict_comprehension.py
@@ -1,8 +1,11 @@
 from cmath import sqrt
 import math
 def run():
-    nums_exp={i:math.sqrt(i) for i in range(1,21)}
-    print(nums_exp)
-
+    dict_nums={i:math.sqrt(i) for i in range(1,6,1)}
+    print(dict_nums)
+def expo():
+    nums_expo=[pow(i,2) for i in range(1,6,1)]
+    print(nums_expo)
 if __name__=='__main__':
     run()
+    expo()
